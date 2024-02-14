e82b080544ddbc18c27f7d7105d05d657181f8e9
Carlos Panduro <cpanduro@inforumsol.com>
2024-02-14 22:54:22 +0000

    project1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageBox) {
+        "use strict";
+
+        return Controller.extend("sap.btp.project1.controller.View1", {
+            onInit: function () {
+
+            },
+            onPress: function () {
+                debugger;
+                MessageBox.alert("You have been alerted!");
+            }
+        });
+    });
